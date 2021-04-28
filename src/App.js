5fcad041d67b216ff2a2be2f4f6744c4893e2108
lamare559 <63024892+lamare559@users.noreply.github.com>
2021-04-28 16:04:35 +0100

    jobstriver project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Feedback from "./Components/Pages/Feedback";
+import Home from "./Components/Pages/Home";
+import JobseekerRegister from "./Components/Pages/Jobseeker/JobseekerRegister";
+import JobseekerSignin from "./Components/Pages/Jobseeker/JobseekerSignin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/feedback" component={Feedback} />
+          <Route path="/register" component={JobseekerRegister} />
+          <Route path="/login" component={JobseekerSignin} />
+        </Switch>
+      </Router>
     </div>
   );
 }
